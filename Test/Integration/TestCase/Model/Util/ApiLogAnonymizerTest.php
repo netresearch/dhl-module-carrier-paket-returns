a3806d46ad4290ea2d89959d569dc313e2b1abd6
Christoph Aßmann <christoph.assmann@netresearch.de>
2020-04-01 20:51:49 +0200

    DHLGW-833: configure log anonymizer

---
+++ Test/Integration/TestCase/Model/Util/ApiLogAnonymizerTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+declare(strict_types=1);
+
+namespace Dhl\PaketReturns\Test\Integration\TestCase\Model\Util;
+
+use Dhl\ShippingCore\Model\Util\ApiLogAnonymizer;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+
+class ApiLogAnonymizerTest extends TestCase
+{
+    /**
+     * @return string[][][]
+     */
+    public function getLogs(): array
+    {
+        return [
+            'return' => [
+                ['message' => file_get_contents(__DIR__ . '/../../../Provider/_files/return_log_orig.txt')],
+                ['message' => file_get_contents(__DIR__ . '/../../../Provider/_files/return_log_anon.txt')],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider getLogs
+     *
+     * @param string[] $originalRecord
+     * @param string[] $expectedRecord
+     */
+    public function stripSensitiveData(array $originalRecord, array $expectedRecord)
+    {
+        /** @var ApiLogAnonymizer $anonymizer */
+        $anonymizer = Bootstrap::getObjectManager()->create(ApiLogAnonymizer::class, ['replacement' => '[test]']);
+        $actualRecord = $anonymizer($originalRecord);
+        self::assertSame($expectedRecord, $actualRecord);
+    }
+}
